a0d9f466c08a36d6c3b0410457f4b59f46aa871c
NUR HASANAH <80372457+ananhs12@users.noreply.github.com>
2021-10-13 21:26:57 +0800

    Add files via upload

---
+++ LatihanFragmentDasar-master/README.md
@@ -0,0 +1,10 @@
+Nim : E41202091
+NAMA : NUR HASANAH
+GOLONGAN : C
+# LatihanFragmentDasar
+Fragment berbeda dari Activity di android studio, fragment sendiri adalah bagian dari user interface atau perilaku sebuah aplikasi yang dapat ditempatkan pada Activity yang memungkinkan untuk mendesain Activity yang modular atau Fragment bisa dikatakan semacam Sub-Activity.
+
+# Screenshoot
+![screenshoot aplikasi](https://user-images.githubusercontent.com/50509675/67206912-bdbce880-f43c-11e9-8878-ece907e3b576.png)
+
+
---
+++ LatihanFragmentDasar-master/app/src/main/java/com/takatutustudio/latihanfragmentdasar/activity/MainActivity.java
@@ -0,0 +1,81 @@
+package com.takatutustudio.latihanfragmentdasar.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.takatutustudio.latihanfragmentdasar.R;
+import com.takatutustudio.latihanfragmentdasar.fragment.About;
+import com.takatutustudio.latihanfragmentdasar.fragment.Help;
+import com.takatutustudio.latihanfragmentdasar.fragment.Home;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+    Button btnHome, btnAbout, btnHelp, btnExit;
+
+    Home fragmentHome;
+    About fragmentAbout;
+    Help fragmentHelp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnHome = findViewById(R.id.btn_home);
+        btnAbout = findViewById(R.id.btn_about);
+        btnHelp = findViewById(R.id.btn_help);
+        btnExit = findViewById(R.id.btn_exit);
+
+        btnHome.setOnClickListener(this);
+        btnAbout.setOnClickListener(this);
+        btnHelp.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+    }
+
+    void menuHome() {
+        fragmentHome = new Home();
+        FragmentTransaction ft = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragmentHome);
+        ft.commit();
+    }
+
+    void menuAbout() {
+        fragmentAbout = new About();
+        FragmentTransaction ft = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragmentAbout);
+        ft.commit();
+    }
+
+    void menuHelp() {
+        fragmentHelp = new Help();
+        FragmentTransaction ft = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragmentHelp);
+        ft.commit();
+    }
+
+    void menuExit() {
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnHome) {
+            menuHome();
+        }
+        if (v == btnAbout) {
+            menuAbout();
+        }
+        if (v == btnHelp) {
+            menuHelp();
+        }
+        if (v == btnExit) {
+            menuExit();
+        }
+    }
+}
